f5452545ec09415e10001162497174204119b46f
Vinicius Flores <viniciusflores93@gmail.com>
2019-11-25 23:23:38 -0300

    Adicionadas queries do relatorio

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -57,5 +57,84 @@ router.get('/countNonAdultMovies',function(req, res) {
   })
 })
 
+router.get('/query1',function(req, res) {
+  query("SELECT PrimaryTitle, LaunchYear FROM Title LIMIT 10;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query2',function(req, res) {
+  query("SELECT *  FROM Episode AS E NATURAL LEFT OUTER JOIN Title LIMIT 10;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query3',function(req, res) {
+  query("SELECT PrimaryTitle, AverageRating FROM Title NATURAL JOIN Rating ORDER BY AverageRating DESC;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query4',function(req, res) {
+  query("SELECT * FROM (SELECT Name, EpisodeID FROM Genres NATURAL JOIN TitleGenre NATURAL JOIN Title NATURAL JOIN Episode ORDER BY Name) AS MovieSummary GROUP BY Name;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query5',function(req, res) {
+  query("SELECT PrimaryTitle, Name FROM Title NATURAL JOIN TitleType NATURAL JOIN Types ORDER BY PrimaryTitle;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query6',function(req, res) {
+  query("SELECT PrimaryTitle FROM Title T1 WHERE TitleID NOT IN (SELECT TitleID FROM Title T2 NATURAL JOIN Episode E) LIMIT 10;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query7',function(req, res) {
+  query("SELECT S1.Name FROM Staff S1 WHERE S1.BirthYear in (SELECT MIN(S2.BirthYear) FROM Staff S2 WHERE S2.BirthYear > 1900);").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query8',function(req, res) {
+  query("SELECT PrimaryTitle, COUNT(EpisodeID) FROM Title T NATURAL JOIN Episode E GROUP BY PrimaryTitle;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query9',function(req, res) {
+  query("SELECT PrimaryTitle, COUNT(StaffID) FROM Title T NATURAL JOIN TitleStaff TS NATURAL JOIN Staff S GROUP BY PrimaryTitle;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
+
+router.get('/query10',function(req, res) {
+  query("SELECT max(tot_episodes) FROM (SELECT TitleID, COUNT(EpisodeID) AS tot_episodes FROM Title NATURAL JOIN Episode GROUP BY TitleID) AS total_titles;").then(r => {
+    res.status(200).send(r)
+  }).catch(e => {
+    res.status(500)
+  })
+})
 
 module.exports = router;
